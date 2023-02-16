c6e6b25ba9a728783d30b7b47b12d8eb56e611c9
Congo Justus Migue <congomigue@gmail.com>
2023-02-16 17:10:08 +0000

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include
+/**
+ * main - Entry point
+ *
+ * Return: with proper grammar, but the outcome is a piece of art
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
