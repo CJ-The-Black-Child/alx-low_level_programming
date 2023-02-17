c919d24478e29a4bdb00d3e4f97e49c620d68b7a
Congo Justus Migue <congomigue@gmail.com>
2023-02-17 22:07:22 +0000

    made changes on file ./8-print_base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
+
 /**
  * main - Entry Point
  *
- * Return -  will return 0 for success
+ *Description: Prints all digits of base 10, followed by all
+ * lowercase letters of base 16
+ *
+ * Return:  will return 0 for success
  */
 int main(void)
 {
@@ -14,7 +16,7 @@ int main(void)
 	for (num = '0'; num <= '9'; num++)
 	putchar(num);
 	for (base_16 = 'a'; base_16 <= 'f'; base_16++)
-		putchar(base_);
-		putchar('\n');
-		return (0);
+		putchar(base_16);
+	putchar('\n');
+	return (0);
 }
