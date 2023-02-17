042264189b12768f9eac523b148a8f947818d478
Congo Justus Migue <congomigue@gmail.com>
2023-02-17 22:42:29 +0000

    made ammends to file 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,14 +9,21 @@
  */
 int main(void)
 {
-	int d;
+int i, j;
 
-	for (d = '0' ; d <= '9' ; d++)
+for (i = 0 ; i <= 9 ; i++)
+{
+	for (j = i + 1; j <= 9; j++)
 	{
-		putchar(d);
-	}
-	putchar(',');
-	putchar(' ');
-	putchar('\n');
-	return (0);
+	putchar(i + '0');
+	putchar(j + '0');
+if (i != 8 || j != 9)
+{
+
+putchar(',');
+putchar(' ');
+}
+}
+}
+return (0);
 }
