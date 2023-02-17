2bd655db875f1c447722c43b64839b4fcdd78791
Congo Justus Migue <congomigue@gmail.com>
2023-02-17 19:50:43 +0000

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - Entry Point
+ *
+ * Return - will return 0 for success
+ */
+int main(void)
+{
+	char alpha_bet;
+
+	for (alpha_bet = 'z' ; alpha_bet >= 'a' ; alpha_bet--)
+		putchar(alpha_bet);
+		putchar('\n');
+		return (0);
+}
