803661e357cd7013a4bae65892d884c506e53501
Congo Justus Migue <congomigue@gmail.com>
2023-02-17 22:55:30 +0000

    final changes for task 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,16 +10,18 @@
 int main(void)
 {
 int i, j;
-
 for (i = 0 ; i <= 9 ; i++)
 {
 	for (j = i + 1; j <= 9; j++)
 	{
 	putchar(i + '0');
 	putchar(j + '0');
-if (i != 8 || j != 9)
+if (i == 8 && j == 9)
 {
-
+putchar('\n');
+}
+else
+{
 putchar(',');
 putchar(' ');
 }
