eb4950b24d59265e16502e7fb675881ea9a673dc
Congo Justus Migue <congomigue@gmail.com>
2023-02-17 19:43:37 +0000

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - Entry Point
+ *
+ * Return - to return 0 for success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+		printf("\n");
+		return (0);
+}
