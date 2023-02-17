ba9e4030ee1a3a23f628502352c584d8ae0d2533
Congo Justus Migue <congomigue@gmail.com>
2023-02-17 19:56:04 +0000

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - Entry Point
+ *
+ * Return -  will return 0 for success
+ */
+int main(void)
+{
+	int num;
+	char base_16;
+
+	for (num = '0'; num <= '9'; num++)
+	putchar(num);
+	for (base_16 = 'a'; base_16 <= 'f'; base_16++)
+		putchar(base_);
+		putchar('\n');
+		return (0);
+}
