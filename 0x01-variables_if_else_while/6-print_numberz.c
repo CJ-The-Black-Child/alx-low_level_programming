63224bc477c6944f0764fc3aac31f847d663987c
Congo Justus Migue <congomigue@gmail.com>
2023-02-17 19:47:15 +0000

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - Entry Point
+ *
+ * Return - will return 0 for success
+ */
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+		putchar(d);
+		putchar('\n');
+		return (0);
+}
