59a8d962653d23f031132dae429bb7f3d733473f
Congo Justus Migue <congomigue@gmail.com>
2023-02-17 21:30:40 +0000

    made changes to file 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,21 +2,20 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - Entry Point
- *
- * Return- to always return 0 for success
+ * main - Entry Point. This is the main function
+ * Description: Prints out the alphabet in lowercase followed by a new line
+ * Return: to always return 0 for success
  */
 int main(void)
 {
-	char alpha_bet, a, b;
+	char alpha_bet;
 
-	a = 'e';
-	b = 'q';
 	for (alpha_bet = 'a'; alpha_bet <= 'z'; alpha_bet++)
 	{
-		if (alpha_bet != a && alpha_bet != b)
+		if (alpha_bet != 'e' && alpha_bet != 'q')
 			putchar(alpha_bet);
 	}
-	printf("\n");
+	putchar('\n');
+
 	return (0);
 }
