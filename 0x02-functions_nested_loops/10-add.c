dc7b1ef5afb88e08b97c6d5b1098c1aec6d041fa
Congo Justus Migue <congomigue@gmail.com>
2023-02-21 22:14:56 +0000

    islower

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+/*
+ * File: 10-add.c
+ */
+#include "main.h"
+/**
+ * add - Adds 2 integers.
+ * @a: The first integer to be added.
+ * @b: the second integer to be added.
+ *
+ * Return: The result of the addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+/*
+ * File: 4-isalpha.c
+ */
+#incldue "main.h"
+/**
+ * _isalpha - checks if a character is alphabetic
+ * @c: The character thats to be checked.
+ *
+ * Return: 1 if the character is letter, lowercase or uppercase, otherwise.
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') ||
+			(c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
