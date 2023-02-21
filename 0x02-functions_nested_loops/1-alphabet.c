6c81d6b9d5da9d8302ed1bc3b9f15b791b8f9070
Congo Justus Migue <congomigue@gmail.com>
2023-02-21 18:10:45 +0000

    made more changes on file 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,7 @@
 #include "main.h"
+
 /**
- * File: 1-alphabet.c
- *  prints the alphabet in lowercase,followed by a new line
- *
+ * print_alphabet -  prints the alphabet in lowercase,followed by a new line
  * Return: Function returns 0
  */
 void print_alphabet(void)
@@ -11,6 +10,6 @@ void print_alphabet(void)
 	char i;
 
 	for (i = 'a'; i <= 'z'; i++)
-		_putchar(i);
+	_putchar(i);
 	_putchar('\n');
 }
