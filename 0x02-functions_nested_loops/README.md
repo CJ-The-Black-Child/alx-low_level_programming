3896ff02bdd576725e1e3cf85bef8df70606f2e8
Congo Justus Migue <congomigue@gmail.com>
2023-02-21 14:35:59 +0000

    0-putchar.c

---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+README FILE for tasks based on 0x02-functions_nested_loops
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * File:  main.h
+ *
+ * Desc: Header File that contains declaration for all my functions
+ *
+ *	 used in the directory of 0x02-functions_nested_loops.
+ */
+int _putchar(char c);
+
+#endif
