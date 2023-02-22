d2446adfb1335d0a04617b4802da721be6830f23
Congo Justus Migue <congomigue@gmail.com>
2023-02-22 02:54:31 +0000

    file 12 changes # The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,42 +3,61 @@
 */
 #include "main.h"
 /**
-* print_times_table - Prints the times table of the input from 0 to 100 .
+* print_times_table - Prints the n times table, starting with 0
 *
 * @n: The values of the times table to be printed.
+*
+* This function prinst the times table from 0 to n, where each
+* row represents the products of the values from 0 to n with the
+* row numbers. For example, if n is for 5, the output will be:
+*
+* 0 0 0 0 0 0
+* 0 1 2 3 4 5
+* 0 2 4 6 8 10
+* 0 3 6 9 12 15
+* 0 4 8 12 16 20
+* 0 5 10 15 20 25
+*
+* Return: void
 */
 void print_times_table(int n)
 {
-int num, mult, prod;
+int i, j, product;
 
-if (n >= 0 && n <= 15)
+if (n < 0 || n > 15)
+return;
+
+for (i = 0; i <= n; i++)
 {
-for (num = 0; num <= n; num++)
+for (j = 0; j <= n; j++)
 {
-for (mult = 0; mult <= n; mult++)
-{
-prod = num * mult;
-if (mult == 0)
-{
-_putchar('0');
-}
-else
+product =  i * j;
+if (j > 0)
 {
 _putchar(',');
 _putchar(' ');
-_putchar((prod / 100) + '0');
 }
-if (prod >= 10)
+
+if (product < 10)
 {
-_putchar(((prod /10) % 10)+ '0');
+_putchar(' ');
+_putchar(' ');
+_putchar(product + '0');
+}
+else if (product < 100)
+{
+_putchar(' ');
+_putchar(product / 10 + '0');
+_putchar(product % 10 + '0');
 }
 else
 {
-_putchar(' ');
+_putchar(product / 100 + '0');
+_putchar((product / 10) % 10 + '0');
+_putchar(product % 10 + '0');
 }
-_putchar((prod % 10) + '0');
 }
 _putchar('\n');
 }
 }
-}
+
