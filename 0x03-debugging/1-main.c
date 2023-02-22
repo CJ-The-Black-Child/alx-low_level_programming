be5f9b61ff984d3c95f940855227c71cbeb8517b
Congo Justus Migue <congomigue@gmail.com>
2023-02-22 10:57:08 +0000

    more changes on file 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,6 +5,8 @@
  * Return: 0
  */
 
+
+
 int main(void)
 
 {
@@ -13,12 +15,10 @@ int i;
 
 printf("Infinite loop incoming :(\n");
 i = 0;
-
 /*while (i < 10)*/
 /*{*/
 /* putchar(i);*/
 /*}*/
 printf("Infinite loop avoided! \\0/\n");
-
 return (0);
 }
