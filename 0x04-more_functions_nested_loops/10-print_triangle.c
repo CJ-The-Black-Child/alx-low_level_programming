9fc5a3cbe9e69359f5b77efddd77d6e32b7e9fd8
Congo Justus Migue <congomigue@gmail.com>
2023-02-23 21:06:07 +0000

    fixed no. 10 triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,9 +12,9 @@ void print_triangle(int size)
 	_putchar('\n');
 }
 
-	for (i = 1; i < size; i++)
+	for (i = 1; i <= size; i++)
 	{
-		for (j = 0; j < size - i - 1; j++)
+		for (j = size; j > i; j--)
 		{
 			_putchar(' ');
 		}
