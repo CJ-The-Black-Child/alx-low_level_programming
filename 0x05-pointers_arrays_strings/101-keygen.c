47d2b868324b931339dba2ee82bc3d8e075e8866
Congo Justus Migue <congomigue@gmail.com>
2023-02-27 21:32:45 +0000

    ammends Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,38 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <math.h>
-
+#include <limits.h>
 /**
- * main - print password.
+ * main - print password. Generates a random valid password for the program
  *
- * Return: 0.
+ * Return: Always 0.
  */
 
 int main(void)
 {
-	int ascii = 2772, i = 0, j, random;
+	int ascii_sum = 2772;
+	int password_index = 0;
+	int random_value;
 	char password[100];
 	time_t t;
 
-	srand((int) time(&t));
-	while (ascii > 126)
-	{
-		random = rand() % 126;
-		password[i] = random;
-		ascii -= random;
-		i++;
+	/* seeds the random number generator */
+	srand((unsigned) time(&t));
+	
+	/* generate random characters till the sum of the ASCII is less 2772 */
+	do {
+		random_value = rand() % (CHAR_MAX + 1);
+		password[password_index] = random_value;
+		ascii_sum -= random_value;
+		password_index++;
 	}
-	if (ascii > 0)
-		password[i] = ascii;
-	else
-	{
-		i--;
-	}
+	while (ascii_sum > 0);
 
-	for (j = 0; j <= i; j++)
+	/* add the final character to the password */
+	password_index--;
+	password[password_index] += ascii_sum;
+
+	/* print the password */
+	for (int i = 0; i <= password_index; i++)
 	{
-		printf("%c", password[j]);
+		printf("%c", password[i]);
 	}
+	printf("\n");
 	return (0);
 }
