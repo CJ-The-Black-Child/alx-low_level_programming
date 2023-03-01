bcf8e1e75b99f99f10893f73aa1e171099cb2256
Congo Justus Migue <congomigue@gmail.com>
2023-03-01 14:37:26 +0000

    Always look up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,11 +9,11 @@ char *string_toupper(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
 		{
-			str[i] = str[i] = 32;
+			str[i] = str[i] - 32;
 		}
 	}
 	return (str);
