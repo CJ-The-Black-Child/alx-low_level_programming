ef1b2cbb74ec17c33d9218c6fb782a1a88cea5eb
Congo Justus Migue <congomigue@gmail.com>
2023-03-01 15:38:30 +0000

    correction on file 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,24 +9,12 @@
 char *_strncat(char *dest, char *src, int n)
 
 {
-	int i, j;
+	int index = 0, dest_len = 0;
+	while (dest[index++])
+		dest_len++;
 
-	i = 0;
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-
-	j = 0;
-
-	while (j < n && src[j] != '\0')
-	{
-		dest[i] = src[j];
-		j++;
-		i++;
-	}
-
-	dest[i] = '\0';
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src [index];
 
 	return (dest);
 }
