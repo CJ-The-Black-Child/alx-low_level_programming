f079c93a696b5e523c206ee652624c102194c5bc
Congo Justus Migue <congomigue@gmail.com>
2023-03-02 04:53:25 +0000

    file 10 fixed

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Description: This program initializes an array and a pointer,
+ * updates the value of an integer variable using pointer arithmetic,
+ * and finally prints the value of an array element.
+ *
+ * Return: Always  (Success)
+ */
 int main(void)
 {
 	int n;
@@ -8,6 +16,12 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
+
+	*((int *)p + 10) = 98;
+	/* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
 	/*
 	* write your line of code here...
 	* Remember:
@@ -16,8 +30,4 @@ int main(void)
 	* - only one statement
 	* - you are not allowed to code anything else than this line of code
 	*/
-	*((int *)p + 10) = 98;
-	/* ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2]);
-	return (0);
-}
+
