28e572db2e35e36de710756f3d0dc508fdb41390
Congo Justus Migue <congomigue@gmail.com>
2023-03-02 16:00:29 +0000

    check 13

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,31 +1,47 @@
 #include "main.h"
-#include <stdio.h>
+
+char *add_strings(char *n1, char *n2, char *r, int r_index);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 /**
- * *infinite_add - adds two numbers
+ * add_strings - Adds the numbers stored in two strings.
  * @n1: The number one to be added.
  * @n2: The number two to be added.
  * @r: The buffer that the function will use to store the result.
- * @size_r: The buffer for size.
+ * @r_index: The current index of the buffer.
  *
  * Return: the pointer to dest.
  */
 
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+char *add_strings(char *n1, char *n2, char *r, int r_index)
 {
-	int index, n1_len = 0, n2_len = 0;
+	int num, tens = 0;
 
-	for (index = 0; *(n1 + index); index++)
-		n1_len++;
-
-	for (index = 0; *(n2 + index); index++)
-		n2_len++;
-
-	if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
+	for (; *n1 && *n2; n1--, n2--, r_index--)
+	{
+		num = (*n1 - '0') + (*n2 - '0');
+		num += tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+	for (; *n1; n1--, r_index--)
+	{
+		num = (*n1 - '0') + tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+	for (; *n2; n2--, r_index--)
+	{
+		num = (*n2 - '0') + tens;
+	       *(r + r_index) = (num % 10) + '0';
+	       tens = num / 10;
+	}
+	if (tens && r_index >= 0)
+	{
+		*(r + r_index) = (tens % 10) + '0';
+		return (r + r_index);
+	}
+	else if (tens && r_index < 0)
 		return (0);
 
-	n1 += n1_len - 1;
-	n2 += n2_len - 1;
-	*(r + size_r) = '\0'
-
-		return (add_strings(n1, n2, r, --size_r));
+	return (r + r_index + 1);
 }
