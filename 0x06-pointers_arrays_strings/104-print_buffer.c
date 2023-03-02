fc6bc3354c22fc078aec20ccdb78c61650e4b306
Congo Justus Migue <congomigue@gmail.com>
2023-03-02 14:11:20 +0000

    more on file 14

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -21,23 +21,25 @@ void print_buffer(char *b, int size)
 			if ((index + byte) >= size)
 				printf(" ");
 			else
-	printf("%02x", *(b + index + byte));
-			if ((index % 2) != 0 && index != 0)
+	printf(" %02x", *(b + index + byte));
+
+			if ((index % 5) == 4)
 				printf(" ");
 		}
+		printf(" ");
+
 		for (index = 0; index < 10; index++)
 		{
 			if ((index + byte) >= size)
 				break;
-			else if (*(b + index + byte) >= 31 &&
-					*(b + index + byte) <= 126)
+
+			 if (*(b + index + byte) >= 32 &&
+					*(b + index +byte) <= 126)
 				printf("%c", *(b + index + byte));
 			else
 				printf(".");
 		}
-		if (byte >= size)
-			continue;
-
+		
 		printf("\n");
 	}
 	if (size <= 0)
