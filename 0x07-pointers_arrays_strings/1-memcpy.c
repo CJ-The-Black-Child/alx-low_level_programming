bad809bd7d3c422860d392a7a4186431f083dd75
Congo Justus Migue <congomigue@gmail.com>
2023-03-06 08:17:52 +0000

    added all the required files for the tasks at hand

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memcpy - Copies memory area
+ *
+ * @dest: Where to cpoy the memory to.
+ * @n: The number of bytes
+ * @src: The memory area to copy from
+ *
+ * Return: A pointer to @dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = o; i < n; i++)
+	
+		dest[i] = src[i];
+
+		return (dest);
+}
