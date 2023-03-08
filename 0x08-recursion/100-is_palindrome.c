c2425264110807cc875573adc84f83383a513106
Congo Justus Migue <congomigue@gmail.com>
2023-03-08 16:11:57 +0000

    made changes to file with palindrome exercise advance task 1

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,7 +2,7 @@
 
 /**
  * _strlen - Return length of string
- * @str: string to check
+ * @str: String to check
  *
  * Return: Length of str.
  */
@@ -12,28 +12,27 @@ int _strlen(char *str)
 	if (*str == '\0')
 		return (0);
 	else
-		return (1 + _strlen(str + 1));
+		return(1 + _strlen(str + 1));
 }
 
 /**
- * check_palindrome -  Checks to see if a string is a palindrome.
- * @l: left hand index.
- * @r: right hand index.
- * @p: possible palindrome.
- *
- * Return: 1 if palindrome 0 if not a palindrome.
+ * check_palindrome - Checks to see if a string is a palindrome
+ * @l: left hand index
+ * @r: right hand index
+ * @p: possible palindrome
+ * 
+ * Return: 1 if palindrome 0 if not a palindrome
  */
 
 int check_palindrome(int l, int r, char *p)
 {
 	if (l >= r)
 		return (1);
-	else if (p[1] != p[r])
+	else if (p[l] != p[r])
 		return (0);
 	else
-		return (check_palindrome(l + 1, r - 1, p));
+		return (check_palindrome(l + 1,r -1,p));
 }
-
 /**
  * is_palindrome - States if a string is a palindrome
  * @s: string to check
