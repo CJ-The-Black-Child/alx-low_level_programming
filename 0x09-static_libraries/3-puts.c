27b940f4922ed5016f33b46fe1c9edb355d0bfd6
Congo Justus Migue <congomigue@gmail.com>
2023-03-10 22:42:53 +0000

    added all the necessary files to git

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+void _puts(char *str)
+#include <stdio.h>
+/**
+ * main - Entry point
+ * puts -  Programming is like building a multilingual puzzle
+ * Return: Always 0 for success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
