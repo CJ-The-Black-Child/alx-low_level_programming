cd45ef57ac786341b8c21dc424fdd3bb044917a0
Congo Justus Migue <congomigue@gmail.com>
2023-03-11 00:51:31 +0000

    Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to a string(s)
+ * Return: 0 if no errors, else 1
+ */
+
+int main(int argc, char *argv[])
+{
+int a, b, product;
+if (argc != 3)
+{
+puts("Error");
+return (1);
+}
+a = atoi(argv[1]);
+b = atoi(argv[2]);
+product = a *b;
+printf("%d\n", product);
+return (0);
+}
