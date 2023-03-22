839a7ea51f34d130cbb410ac64e215dcc8b04f57
Congo Justus Migue <congomigue@gmail.com>
2023-03-22 13:43:08 +0000

    made the codes betty compliant and fixed file 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,21 +1,22 @@
 #include "function_pointers.h"
-#include <stdio.h>
-/*
- * array_iterator - prints each array element on a new line
- * @array: array
- * @size: how many element to print
- * @action: pointer to print in regular or hex
+
+/**
+ * array_iterator - executes a func given as a param on each element of array
+ * @array: array to execute func on
+ * @size: size of array
+ * @action: pointer to the func to use
+ *
  * Return: void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int i;
+	size_t i;
 
-	if (array ==NULL || action == NULL)
-		return;
-
-	for (i = 0; i < size; i++)
+	if (array && action)
 	{
-		action(array[i]);
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
 	}
 }
