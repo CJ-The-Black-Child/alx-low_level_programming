71094a02df01d6a01101803958e20dafc06f8d83
Congo Justus Migue <congomigue@gmail.com>
2023-03-31 02:41:56 +0000

    fixed runtime errors

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include "lists.h"
 
- /**
+/**
   * print_list - prints all the elements of a linked list
   * @h: pointer to the list_t list to print
+  *
   * Return: The number of nodes printed
   */
 
@@ -11,10 +12,10 @@ size_t print_list(const list_t *h)
 {
 	size_t s = 0;
 
-	while (h != NULL)
+	while (h)
 	{
-		if (!h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
+		if (!h->str)
+			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
