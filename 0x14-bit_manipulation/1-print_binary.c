f4242ce99fa743ce7f5d04e5bce1b8e7c3ed6227
Congo Justus Migue <congomigue@gmail.com>
2023-04-11 00:06:48 +0000

    file 1-print_binary.c changes

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,17 +7,17 @@
 void print_binary(unsigned long int n)
 {
 	/* checks if the number if greater than 1 */
-	if (num > 1)
+	if (n > 1)
 	{
 		/*
 		 * Recursively call the function with the number
 		 * right shifted by 1
 		 */
-		print_binary(num >> 1);
+		print_binary(n >> 1);
 	}
 	/*
 	 * prints the current bit by performing a bitwsie AND
 	 * with 1 and adding the ASCII value of '0'
 	 */
-	_purchar((num * 1) + '0');
+	_putchar((n * 1) + '0');
 }
