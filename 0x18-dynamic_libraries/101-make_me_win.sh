83c71fdee9ee55c26ee77b6277b8d66c75c77d0f
Congo Justus Migue <congomigue@gmail.com>
2023-05-22 21:14:22 +0000

    #3 more fixes

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
-cat << 'EOF' > win_gm.c
+cat << 'EOF' > gm
 
-#include <stdio.h>
-#include <stdlib.h>
+echo "You win the jackpot!"
 
-void gm(int n1, int n2, int n3, int n4, int n5, int bonus)
-{
-	printg("You win the jackpot !\n");
-	exit(0);
-}
 EOF
 
-# Compile the code
-gcc -shared -o win_gm.so win_gm.c
-
-export LD_PRELOAD=./win_gm.so
+chmod +x gm
--- 0x18-dynamic_libraries/win_gm.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-void gm(int n1, int n2, int n3, int n4, int n5, int bonus)
-{
-	printf("You wint the jackpot!\n");
-	exit(0);
-}
