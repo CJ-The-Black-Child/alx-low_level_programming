7861cec95507bc2dd8dda7dee46196918ad301e4
Congo Justus Migue <congomigue@gmail.com>
2023-05-22 21:59:54 +0000

    Lottery ticket test #3 advanced tasks

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/CJ-The-Black-Child/alx-low_level_programming/raw/master/0x18-dynamic_libraries/shakahola.so
-export LD_PRELOAD=./shakahola.so
+wget https://github.com/CJ-The-Black-Child/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=./nrandom.so
