f1e7d68c2458f23d1d95fd16c55c5b6fb2fc04a0
Congo Justus Migue <congomigue@gmail.com>
2023-06-09 13:18:33 +0000

    #task; 1. djb2

---
+++ 0x1A-hash_tables/hash_tables.h
@@ -0,0 +1,27 @@
+#ifndef HASH_TABLES_H
+#define HAS_TABLES_H
+
+#include <stdlib.h>
+
+/* Data Sstructure for hash table */
+typedef struct has_tables_s
+{
+	unsigned long int size;
+	struct hash_node_s **array;
+} hash_table_t;
+
+/* Data structure for hash node */
+typedef struct hash_node_s
+{
+	char *key;
+	char *value;
+	struct hash_node_s *next;
+} hash_node_t;
+
+/* Function to create a hash table */
+hash_table_t *hash_table_create(unsigned long int size);
+
+/* Fucntion to calculate the hash value using djb2 algorithm */
+unsigned long int hash_djb2(const unsigned char *str);
+
+#endif /* HASH_TABLES_H*/
