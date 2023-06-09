7e8f5a8fb7109fbdea29e3e8e9da5805b7f9f25f
Congo Justus Migue <congomigue@gmail.com>
2023-06-09 14:30:54 +0000

    fixed betty coding styling and syntaxing issues

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,7 +12,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 unsigned long int index;
 hash_node_t *new_node, *current_node;
-
 if (ht == NULL || key == NULL || *key == '\0')
 return (0);
 index = key_index((unsigned char *) key, ht->size);
