4029f0b89d092104aa72ae1ae434c1eacf25a16f
CJ-The-Black-Child <congomigue@gmail.com>
2023-09-12 10:15:41 +0000

    Function to search a value using the Exponential search algorithm

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -22,7 +22,7 @@ int jump_search(int *array, size_t size, int value)
 
 	while (right < size && array[right] < value)
 	{
-		printf("Value checked array[%lu] = [%d]\n", right,array[right]);
+		printf("Value checked array[%lu] = [%d]\n", right, array[right]);
 		left = right;
 		right += step;
 	}
