744b6b90ab2b46bb1ef3991237086f3719ce30f9
CJ-The-Black-Child <congomigue@gmail.com>
2023-09-13 01:52:28 +0000

    Jump search in a singly linked list

---
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -0,0 +1,42 @@
+#include "search_algos.h"
+
+/**
+ * jump_list - Searches for a value in a sorted list of integers using the
+ * Jump search algorithm.
+ *
+ * @list: Pointer to the head of the list to search in.
+ * @size The number of nodes in the list.
+ * @value: The value to search for.
+ *
+ * Return: A pointer to the first node where value is located, or
+ * -1 (NULL) if not found
+ */
+
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+	size_t step = sqrt(size);
+	listint_t *current = list, *prev = NULL;
+
+	if (!list)
+		return (NULL);
+
+	while (current->index < size & current->n < value)
+	{
+		prev = current;
+		for (size_t i = 0; i < step && current->next; i++)
+			current = current->next;
+		printf("Value checked at index [%lu] = [%d]\n", current->index, current->n);
+	}
+
+	printf("Value found between indexes [%lu] and [%lu]\n",prev->index, current->index);
+	current = prev;
+	while (current && current->index < size)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", current->index, current->n);
+		if (current->n == value)
+			return (current);
+		current = current->next;
+	}
+
+	return (NULL);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,18 @@
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
 
+/* Usable std libraries */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/* Sttuct for singly linked list*/
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
 /* Function prototypes */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
@@ -8,6 +20,11 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+listint_t *create_list(int *array, size_t size);
+void print_list(const listint_t *list);
+void free_list(listint_t *list);
+void free_list(listint_t *list);
 
 
 #endif /* SEARCH_ALGOS_H */
